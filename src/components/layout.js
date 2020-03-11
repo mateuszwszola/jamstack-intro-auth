2b9be322b408b6396c063bef086958e12585dda1
Mateusz Wszola <wszola.m@gmail.com>
2020-03-11 12:59:22 +0100

    Add netlify identity url

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import { IdentityContextProvider } from 'react-netlify-identity-widget';
 import './layout.css';
 
 const Layout = ({ children }) => (
-  <IdentityContextProvider>
+  <IdentityContextProvider url="https://jamstack-auth-test.netlify.com">
     <header>
       <Link to="/">JAMstack App</Link>
     </header>
